a2461aceee64952b0d1f66fd22e0d70cc624078b
caio peliz <cardosojoao418@gmail.com>
2026-06-10 20:37:47 -0300

    alt

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost/vettrace"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY")
